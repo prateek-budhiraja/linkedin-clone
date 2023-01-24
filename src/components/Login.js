66f69134ef6213098ff24e0c28a1944347f72425
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-24 22:56:08 +0530

    added hover styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,15 +8,18 @@ export default function Login() {
 					<img
 						src="/images/linkedin.svg"
 						alt="linkedin logo"
-						className="w-[100px] md:w-[150px] lg:w-[200px]"
+						className="w-[100px] md:w-[150px]"
 					/>
 				</a>
-				<div className="flex gap-5 items-center lg:gap-10">
-					<a className="inline-block md:text-xl lg:text-2xl" href="/signup">
+				<div className="flex gap-3 items-center lg:gap-10">
+					<a
+						className="inline-block md:text-xl border-primaryBlue border-solid px-4 py-2 rounded-full hover:bg-gray-200"
+						href="/signup"
+					>
 						Join Now
 					</a>
 					<a
-						className="inline-block border-2 font-semibold text-primaryBlue border-primaryBlue border-solid px-4 py-2 rounded-full md:text-xl lg:text-2xl"
+						className="inline-block border-2 font-semibold text-primaryBlue hover:text-white hover:bg-primaryBlue border-primaryBlue border-solid px-4 py-2 rounded-full md:text-xl"
 						href="/"
 					>
 						Sign in
@@ -33,13 +36,13 @@ export default function Login() {
 					<h1 className="text-center lg:text-left text-primaryBlue text-2xl mb-5 md:text-5xl lg:text-6xl">
 						Welcome to your professional community
 					</h1>
-					<button className="block mx-auto items-center my-5 py-2 px-5 border-solid border-2 border-black rounded-full md:mt-[40px] lg:mt-[60px] lg:mx-0">
+					<button className="block hover:bg-black text-gray-700 hover:text-white mx-auto items-center my-5 py-2 px-5 border-solid border-2 border-black rounded-full md:mt-[40px] lg:mt-[60px] lg:mx-0">
 						<img
 							className="inline-block mr-2 w-[25px] md:mb-[4px] md:w-[28px] lg:w-[30px]"
 							src="/images/google-g.png"
 							alt="google logo"
 						/>
-						<span className="font-semibold text-gray-700 md:text-xl lg:text-2xl">
+						<span className="font-semibold md:text-xl lg:text-2xl">
 							Sign in with Google
 						</span>
 					</button>
