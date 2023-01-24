733cd18101618726fe09bc4a6431de0337fc3d44
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-24 22:56:21 +0530

    home page init commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
 import Login from "./components/Login";
 
 function App() {
@@ -7,6 +9,15 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route exact path="/" element={<Login />} />
+					<Route
+						path="/home"
+						element={
+							<>
+								<Header />
+								<Home />
+							</>
+						}
+					/>
 				</Routes>
 			</BrowserRouter>
 		</>
