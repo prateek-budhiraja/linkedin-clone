b6d87aa315792d4c060e1e9057436fe63d3c5d04
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-25 17:15:55 +0530

    added styles for larger screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,12 @@ import React from "react";
 export default function Header() {
 	return (
 		<>
-			<nav className="px-3 py-1 bg-white border-b-2 border-solid">
-				<div className="grid grid-cols-5 items-center">
-					<a className="col-span-1 justify-self-start" href="/home">
+			<nav className="px-3 lg:px-5 py-1 lg:py-3 bg-white border-b-2 border-solid">
+				<div className="grid grid-cols-5 lg:grid-cols-10 items-center">
+					<a
+						className="col-span-1 justify-self-start lg:justify-self-center"
+						href="/home"
+					>
 						<img
 							className="min-w-[40px] inline"
 							src="/images/home-logo.svg"
@@ -25,7 +28,44 @@ export default function Header() {
 							placeholder="Search"
 						/>
 					</span>
-					<a className="justify-self-end col-span-1" href="/home">
+					<div className="border-t-2 lg:border-0 border-solid bg-white border-gray-200 absolute lg:static col-span-5 flex w-[100%] justify-evenly bottom-0 left-0 px-1 py-2">
+						<a href="/home">
+							<img className="mx-auto" src="/images/nav-home.svg" alt="home" />
+							<span className="text-sm lg:text-base">Home</span>
+						</a>
+						<a href="/home">
+							<img
+								className="mx-auto"
+								src="/images/nav-network.svg"
+								alt="home"
+							/>
+							<span className="text-sm lg:text-base">My Network</span>
+						</a>
+						<a href="/home">
+							<img className="mx-auto" src="/images/nav-jobs.svg" alt="home" />
+							<span className="text-sm lg:text-base">Jobs</span>
+						</a>
+						<a href="/home">
+							<img
+								className="mx-auto"
+								src="/images/nav-messaging.svg"
+								alt="home"
+							/>
+							<span className="text-sm lg:text-base">Messaging</span>
+						</a>
+						<a href="/home">
+							<img
+								className="mx-auto"
+								src="/images/nav-notifications.svg"
+								alt="home"
+							/>
+							<span className="text-sm lg:text-base">Notification</span>
+						</a>
+					</div>
+					<a
+						className="justify-self-end col-span-1 lg:justify-self-center"
+						href="/home"
+					>
 						<img
 							className="min-w-[40px] inline rounded-full"
 							src="/images/user.svg"
@@ -39,36 +79,6 @@ export default function Header() {
 								alt=""
 							/>
 						</div>
-					</a>
-				</div>
-				<div className="border-t-2 border-solid bg-white border-gray-200 absolute flex w-[100%] justify-evenly bottom-0 left-0 px-1 py-2">
-					<a href="/home">
-						<img className="mx-auto" src="/images/nav-home.svg" alt="home" />
-						<span className="text-sm">Home</span>
-					</a>
-					<a href="/home">
-						<img className="mx-auto" src="/images/nav-network.svg" alt="home" />
-						<span className="text-sm">My Network</span>
-					</a>
-					<a href="/home">
-						<img className="mx-auto" src="/images/nav-jobs.svg" alt="home" />
-						<span className="text-sm">Jobs</span>
-					</a>
-					<a href="/home">
-						<img
-							className="mx-auto"
-							src="/images/nav-messaging.svg"
-							alt="home"
-						/>
-						<span className="text-sm">Messaging</span>
-					</a>
-					<a href="/home">
-						<img
-							className="mx-auto"
-							src="/images/nav-notifications.svg"
-							alt="home"
-						/>
-						<span className="text-sm">Notification</span>
 					</a>
 				</div>
 			</nav>
