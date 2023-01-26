462a40df7298b04d1af764a7ec314c181b712606
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-26 13:21:48 +0530

    added Home hero and optimized it for all screens

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -1,5 +1,42 @@
 import React from "react";
+import Post from "./Post";
 
 export default function HomeHero() {
-	return <div>HomeHero</div>;
+	return (
+		<>
+			<div className="py-4 lg:py-6 px-3 lg:px-8 border-2 border-solid rounded-xl border-gray-500 bg-white mb-4">
+				<div className="flex gap-3 lg:gap-5 items-center">
+					<img
+						className="rounded-full w-[40px]"
+						src="/images/user.svg"
+						alt=""
+					/>
+					<input
+						type="text"
+						className="w-[100%] border-[1px] border-black border-solid rounded-full py-1 px-3 font-semibold"
+						placeholder="Start a post"
+					/>
+				</div>
+				<div className="flex justify-between mt-4">
+					<div className="flex items-center">
+						<img src="/images/photo-icon.svg" alt="" />
+						<span className="lg:text-base text-xs">Photo</span>
+					</div>
+					<div className="flex items-center">
+						<img src="/images/video-icon.svg" alt="" />
+						<span className="lg:text-base text-xs">Video</span>
+					</div>
+					<div className="flex items-center">
+						<img src="/images/event-icon.svg" alt="" />
+						<span className="lg:text-base text-xs">Event</span>
+					</div>
+					<div className="flex items-center">
+						<img src="/images/write-icon.svg" alt="" />
+						<span className="lg:text-base text-xs">Write articles</span>
+					</div>
+				</div>
+			</div>
+			<Post />
+		</>
+	);
 }
